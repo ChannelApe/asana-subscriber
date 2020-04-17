9014bc11533626aa9516f527db43963226355c7c
Michael Averto <mjaverto@channelape.com>
2020-04-17 06:54:31 -0400

    Change console.info to logger

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.post('/receive-webhook/task-added', (req, res) => {
             if(event.resource.resource_type === 'task' && event.action === 'added' && event.parent.resource_type === 'task'){
                 getTaskById(event.resource.gid).then(task => {
                     if(task){
-                        console.info('its a subtask with parent resource_type of task')
+                        info('its a subtask with parent resource_type of task')
                         getTaskById(task.parent.gid).then(parentTask => {
                             addProjectOnSubtask(task, parentTask);
                         });
